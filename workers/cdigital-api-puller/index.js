ae7f72147175e706a39220fb7ed060c9a8366ccf
Franco-Poveda <franco.poveda@gmail.com>
2018-06-29 04:05:11 -0300

    added 1st functional module workers & services docker-compose

---
+++ workers/cdigital-api-puller/index.js
@@ -0,0 +1,71 @@
+var amqp = require('amqplib');
+var pool = require('./db');
+var mysql = require('mysql');
+var moment = require('moment');
+
+
+
+var keys = ["info"];
+amqp.connect('amqp://localhost').then(function (conn) {
+    process.once('SIGINT', function () { conn.close(); });
+    return conn.createChannel().then(function (ch) {
+        var ex = 'topic_logs';
+        var ok = ch.assertExchange(ex, 'topic', { durable: true });
+
+        ok = ok.then(function () {
+            return ch.assertQueue('q0', { durable: true });
+        });
+
+        ok = ok.then(function (qok) {
+            var queue = qok.queue;
+            return ch.bindQueue(queue, ex, "info")
+                .then(function () { return queue; });
+        });
+
+        ok = ok.then(function (queue) {
+            
+            return ch.consume(queue, logTransactions, { noAck: false });
+        });
+        return ok.then(function () {
+            console.log(' [*] Waiting for logs. To exit press CTRL+C.');
+        });
+        function logTransactions(msg) {
+            var tarr = JSON.parse(msg.content.toString());
+            tarr.pop();
+            pool.getConnection(function (err, connection) {
+                if (err) throw err; // not connected!
+                connection.beginTransaction(function (err) {
+                    if (err) { throw err; }
+                    var qvalues = tarr.map(t => {
+                        var set = t.slice(0, -2);
+                        var time = set.splice(2,1);
+                        set[1] = moment(set[1]+time[0],'DDMMYYYYkkmmss').format('YYYY-MM-DD kk:mm:ss');
+                        return set;
+                    });
+                        var query = "INSERT INTO `Transactions` (`type`,`tdate`,`amount`,`barcode`,`ref`,`method`,`cduid`) VALUES ";
+                        var pvalues =  mysql.escape(qvalues)
+                        query = query + pvalues;//[1,"2018-06-20 12:00:00",100, "01909124354188", "ejemplo referencia 1 cliente dni 11111111", "PagoFacilPRUEBA", "66a3bfdfff68f605ce460b7f71ef79a642f10d9ae112077e0dd2dda895b3f5cc"]);
+                        console.log(query);
+                        connection.query(query, function (error, results, fields) {
+                            if (error) {
+                                return connection.rollback(function () {
+                                    throw error;
+                                });
+                            }
+                            connection.commit(function (err) {
+                                if (err) {
+                                    return connection.rollback(function () {
+                                        throw err;
+                                    });
+                                }
+                                console.log('success!');
+                                ch.sendToQueue('q1',Buffer.from(JSON.stringify(qvalues)));
+                                ch.ack(msg);
+                            });
+                        });
+                    });
+                });
+        }
+    });
+}).catch(console.warn);
+
---
+++ workers/cdigital-wh-handler/index.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const morgan = require('morgan');
+const express = require('express');
+const bodyParser = require('body-parser')
+const http = require('http');
+const CSV = require('csv-string');
+var amqp = require('amqplib');
+
+const app = express()
+app.use(morgan('combined'))
+let outch;
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/csv' }))
+app.post('/', (req, res) => {
+  const arr = CSV.parse(req.body);
+
+  console.log(arr)
+  res.send('');
+  outch.publish('topic_logs', 'info', Buffer.from(JSON.stringify(arr)));
+  console.log(" [x] Sent %s:'%s'", 'info', arr);
+});
+  app.get('/', (req, res) => res.send('Hello!'))
+
+const server = http.createServer(app)
+
+
+
+
+let ip = process.env.IP || "0.0.0.0";
+let port = process.env.PORT || 3003;
+
+amqp.connect('amqp://localhost').then(function(conn) {
+  conn.on('error', err => {
+    console.log('error', `[AMQP]: [${err.message}]`);
+});
+conn.on('close', () => {
+    console.log('info', '[AMQP]: connection closed');
+});
+  return conn.createChannel().then(function(ch) {
+    var ex = 'topic_logs';
+    var ok = ch.assertExchange(ex, 'topic', {durable: true});
+    outch = ch;
+    return ok.then(function() {
+      server.listen(port, ip, () => {
+        console.log('[SERVER] WebHook API listening on http://%s:%d', ip, port)
+      });
+    });
+  })
+}).catch(console.log);
+
